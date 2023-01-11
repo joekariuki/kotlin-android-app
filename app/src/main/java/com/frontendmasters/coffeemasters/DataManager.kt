c00d9855b635130eb492b8bb9b63577c40b8b8c3
Joe Kariuki <jknguru6@gmail.com>
2023-01-11 10:13:37 +0300

    Add cart functions

--- app/src/main/java/com/frontendmasters/coffeemasters/DataManager.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/DataManager.kt
@@ -1,5 +1,36 @@
 package com.frontendmasters.coffeemasters
 
-class DataManager {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
+
+class DataManager {
+    var menu: List<Category> by mutableStateOf(listOf())
+    var cart: List<IteminCart> by mutableStateOf(listOf())
+
+    fun cartAdd(product: Product) {
+        var found = false
+        cart.forEach {
+            if (product.id==it.product.id) {
+                it.quantity++
+                found = true
+            }
+        }
+
+        if (!found) {
+            cart = listOf(*cart.toTypedArray(), IteminCart(product, 1))
+        }
+    }
+
+    fun clear() {
+        cart = listOf()
+    }
+
+
+    fun cartRemove(product: Product) {
+        val aux = cart.toMutableList()
+        aux.removeAll { it.product.id==product.id }
+        cart = listOf(*aux.toTypedArray())
+    }
 }
